8549c83703c527c72680893e452a7272edfdf2a8
Sridhar Jammalamadaka <sriavr@users.noreply.github.com>
2016-09-14 18:18:52 +0530

    Added PDFs
    Yet to add Other folder for moving folders which are not extracted from .zip

--- main.py
+++ main.py
@@ -8,22 +8,24 @@ if not directory.endswith('/'):
     directory += '/'
 
 #General File Type Extentions
-apps      = ("Applications", ['.exe'])
+apps      = ("Software", ['.exe','.msi'])
 code      = ("Code", ['.py','.java','.c','.cpp','.rb','.asm','.php','.html',
              '.css','.js','.lua','.jar','.o','.obj'])
 
-music     = ("Music", ['.mp3','.ogg','.wav'])
+music     = ("Songs", ['.mp3','.ogg','.wav'])
 videos    = ("Videos", ['.mp4','.3gp','.avi'])
 pictures  = ("Pictures", ['.jpg','.jpeg','.png','.bmp','.gif'])
-archives  = ("Archives", ['.zip','.rar','.7zip','.tar','.iso'])
+archives  = ("Zip Files", ['.zip','.rar','.7zip','.tar','.iso'])
 
-documents = ("Documents", ['.docx','.doc','.pdf','.txt','.ppt','.pptx','.ppsx','.pptm',
+documents = ("Documents", ['.docx','.doc','.txt','.ppt','.pptx','.ppsx','.pptm',
              '.docm','.dotx','.dotm','.docb','.xlsx','.xlsm','.xltx',
              '.xltm','.xlsb','.xla','.xlam','.xll','.xlw',
              '.ACCDB','.ACCDE','.ACCDT','.ACCDR','.pub',
              '.potx','.potm','.ppam','.ppsm','.sldx','.sldm'])
 
-allTypes = [apps, code, music, videos, pictures, archives, documents]
+pdf     = ("PDFs", ['.pdf','.epub'])
+			 
+allTypes = [apps, code, music, videos, pictures, archives, documents, pdf]
 
 #Check each file in the directory
 
@@ -33,7 +35,8 @@ for fil in listdir(directory):
             if fil.endswith(x) or fil.endswith(x.upper()):
                 if not path.exists(directory+typ[0]+'/'):
                     makedirs(directory+typ[0]+'/')
-                rename(directory+fil, directory+typ[0]+'/'+fil)
+                print(directory+fil + ", " + directory+typ[0]+'/'+fil)
+		rename(directory+fil, directory+typ[0]+'/'+fil)
 
 print ""
 print "How to Find What We Organised:"
@@ -41,7 +44,8 @@ print "Code Folder         - Contains Code, Object Files and jar Files."
 print "Music Folder        - Contains Audio Files."
 print "Videos Folder       - Contains Video Files."
 print "Pictures Folder     - Contains Image Files."
-print "Archives Folder     - Contains rar/zip Files and Other Archives."
+print "Zip Files     - Contains rar/zip Files and Other Archives."
 print "Documents Folder    - Contains Powerpoint and Documents from MS Office."
 print "Applications Folder - Contains applications and executable programs."
+print "PDFs                - Contains PDF/epub files."
 print "Other Folder        - Contains files that were not sorted into a folder."
